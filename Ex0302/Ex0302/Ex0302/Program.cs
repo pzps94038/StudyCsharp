885b991c863942a30653e5ec83d98617eefb65f4
羅雲聘 <pzps94038@yahoo.com.tw>
2021-08-23 07:37:23 +0800

    ex03~04

---
+++ Ex0302/Ex0302/Ex0302/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+namespace Ex0302
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //如果宣告float及decimal需加後置字元
+            float num1 = 1.2233445566778899F;
+            double num2 = 1.2233445566778899;
+            decimal num3 = 1.2233445566778899M;
+            WriteLine($"Float :{num1}");
+            WriteLine($"Double :{num2}");
+            WriteLine($"Decimal :{num3}");
+            //輸出4位小數的數值
+            WriteLine($"Float:{num1:f4}");
+            ReadKey();
+        }
+    }
+}
---
+++ Ex0303/Ex0303/Ex0303/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using static System.Console;
+
+namespace Ex0303
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char chM = 'M';
+            int num1 = (int)chM;
+            int num2 = 78;
+            char chN = Convert.ToChar(78);
+            //輸出結果
+            //字元轉換ASCII碼
+            WriteLine($"字元{chM}，ASCII值 = {num1}");
+            //ASCII碼轉換字元
+            WriteLine($"ASCII{num2}，字元{chN}");
+            ReadKey();
+        }
+    }
+}
